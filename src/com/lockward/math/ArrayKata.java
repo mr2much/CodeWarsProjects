87ee77151ece073066d7140230cdcf286b1e778f
Anubis <anubis.lockward@gmail.com>
2018-07-25 22:15:30 -0400

    Finds the missing Character without using alphabet String.

--- src/com/lockward/math/ArrayKata.java
+++ src/com/lockward/math/ArrayKata.java
@@ -1,20 +1,14 @@
 package com.lockward.math;
 
 public class ArrayKata {
-    private static String alphabet = "abcdefghijklmnopqrstuvwxyz";
     public static char findMissingLetter(char[] chars) {
-        String newAlphabet = Character.isUpperCase(chars[0]) ? alphabet.toUpperCase() : alphabet;
-        
-        int index = newAlphabet.indexOf(chars[0]);
-
-        for (int i = 0; i < chars.length; i++) {
-            if (chars[i] != newAlphabet.charAt(index)) {
-                return newAlphabet.charAt(index);
+        int i;
+        for (i = 1; i < chars.length; i++) {
+            if (chars[i] - chars[i - 1] != 1) {
+                break;
             }
-
-            index++;
         }
 
-        return '\0';
+        return (char)(chars[i] - 1);
     }
 }

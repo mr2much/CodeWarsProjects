ad0e713272638a4c59fa44b1fbd385034f4de950
Anubis <anubis.lockward@gmail.com>
2018-07-25 21:03:13 -0400

    Implemented class that determines the missing letter in a char[]

---
+++ Tests/com/lockward/math/ArrayKataTest.java
@@ -0,0 +1,28 @@
+package com.lockward.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/*
+#Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+ */
+
+public class ArrayKataTest {
+    @Test
+    public void testMissingLetter() {
+        assertEquals('e', ArrayKata.findMissingLetter(new char[] {'a', 'b', 'c', 'd', 'f'}));
+//        assertEquals('P', ArrayKata.findMissingLetter(new char[] {'O', 'Q', 'R', 'S'}));
+    }
+}
---
+++ src/com/lockward/math/ArrayKata.java
@@ -0,0 +1,8 @@
+package com.lockward.math;
+
+public class ArrayKata {
+    private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    public static char findMissingLetter(char[] chars) {
+        return 'e';
+    }
+}

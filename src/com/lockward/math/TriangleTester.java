463d322802f3cff2c92d9d6f6b895a23e5978596
Anubis <anubis.lockward@gmail.com>
2018-07-25 20:50:40 -0400

    Reduced code in isTriangle to just one line.

--- src/com/lockward/math/TriangleTester.java
+++ src/com/lockward/math/TriangleTester.java
@@ -2,10 +2,6 @@ package com.lockward.math;
 
 public class TriangleTester {
     public static boolean isTriangle(int a, int b, int c) {
-        boolean ab = a + b > c;
-        boolean ac = a + c > b;
-        boolean cb = c + b > a;
-
-        return ab && ac && cb;
+        return (a + b > c) && (a + c > b) && (c + b > a);
     }
 }
